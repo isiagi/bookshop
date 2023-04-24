4d0fc32ae027f8cdc3fab9a3a0fa046ae7fec256
isiagi <isiagigeofrey0@gmail.com>
2023-04-24 15:29:21 +0300

    refactor: Add seller, top, book comps

--- src/components/seller/Seller.tsx
+++ src/components/seller/Seller.tsx
@@ -1,11 +1,15 @@
 import { Box, Text } from "@chakra-ui/react";
 import Multi from "../multi/Multi";
+import { useFetchBooks } from '../../hooks/data';
 
 function Seller() {
+  const data =  useFetchBooks()
+  console.log(data);
+  
   return (
     <Box px={"10"} py={27}>
       <Text>Best Seller</Text>
-      <Multi seller={false} speed={3000}/>
+      <Multi seller={false} speed={3000} sellerData={false}/>
     </Box>
   );
 }
