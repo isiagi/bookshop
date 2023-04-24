ec1853821465b1abad5b65fd761daedc7c583228
isiagi <isiagigeofrey0@gmail.com>
2023-04-24 15:36:51 +0300

    chore: clean components

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -4,7 +4,7 @@ import img from "../../assets/book.jpg";
 function Hero() {
   return (
     <Box
-    bgImage={`linear-gradient(rgba(0, 0, 0, 0.527),rgba(0, 0, 0, 0.5)) , url(${img})`}
+    bgImage={`linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3)) , url(${img})`}
       h={"90vh"}
       bgPosition="center"
       bgRepeat="no-repeat"
@@ -13,8 +13,8 @@ function Hero() {
       <Box pl={36} h={'100%'}>
         <Flex align={'center'} h='100%'>
           <Box>
-            <Text fontSize='3xl'>Welcome To BookShop</Text>
-            <Text>Broswer over 200 Books</Text>
+            <Text fontSize='4xl' color={"#fff"}>Welcome To BookShop</Text>
+            <Text fontSize='2xl' color={"#fff"}>Broswer over 200 Books</Text>
             <Button>Broswer Collection</Button>
           </Box>
         </Flex>
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,18 +1,11 @@
-import { Box, Flex, IconButton, Input, Text} from "@chakra-ui/react";
-import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
-
-
-
+import { Box, Flex, Input, Text } from "@chakra-ui/react";
 import { IoBookSharp } from "react-icons/io5";
 import { BsCart4 } from "react-icons/bs";
-import { FaRegUser } from "react-icons/fa";
-import { FaRegIdCard } from "react-icons/fa";
-import { AiOutlineLogin } from "react-icons/ai";
-import SignModel from "../signUpModal/SignModel";
 import MenuComponent from "../menu/MenuComponent";
+import { Link } from "react-router-dom";
 
 function Nav() {
-  
+
   return (
     <Flex
       justifyContent={"space-between"}
@@ -20,30 +13,24 @@ function Nav() {
       p="2"
       borderBottom="1px"
       borderColor="gray.100"
+      wrap={"wrap"}
     >
-      <Flex align="center">
-        <IoBookSharp />
-        <Text>BookShop</Text>
-      </Flex>
+      <Link to="/">
+        <Flex align="center">
+          <IoBookSharp />
+          <Text>BookShop</Text>
+        </Flex>
+      </Link>
       <Box>
-        <Input placeholder="Basic usage" />
+        <Input placeholder="Search by Full Title or Author" />
       </Box>
       <Box>
         <Flex align="center" gap={2}>
-          <BsCart4 />
+          <Link to="/checkout">
+            <BsCart4 style={{ fontSize: "30px" }} />
+           
+          </Link>
           <Box>
-            {/* <Menu>
-              <MenuButton
-                as={IconButton}
-                aria-label="Options"
-                icon={<FaRegUser />}
-                variant="outline"
-              />
-              <MenuList>
-                <MenuItem icon={<AiOutlineLogin />} onClick={() => <SignModel />}>Login</MenuItem>
-                <MenuItem icon={<FaRegIdCard />}>Sign Up</MenuItem>
-              </MenuList>
-            </Menu> */}
             <MenuComponent />
           </Box>
         </Flex>
