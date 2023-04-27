2b2a20b3a200580b603e370e179d7189c1e8206b
isiagi <isiagigeofrey0@gmail.com>
2023-04-27 15:42:22 +0300

    feat: Add admin page

---
+++ src/pages/admin/AdminBook.tsx
@@ -0,0 +1,58 @@
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { deleteBook, useFetchBooks } from "../../hooks/booksApiCalls";
+import { AiOutlineDelete } from "react-icons/ai";
+
+function AdminBook() {
+  const data = useFetchBooks();
+
+  const handleBookDelete = async (itemId: string) => {
+    await deleteBook(itemId);
+    alert("hello");
+  };
+
+  return (
+    <Flex direction="column" justify={"center"} align="center">
+      {data.map((item) => (
+        <Flex
+          gap={10}
+          mb={4}
+          border={"1px solid gray"}
+          w={600}
+          borderRadius={5}
+        >
+          <Box>
+            <Image
+              src={item.imageUrl}
+              alt=""
+              boxSize="200px"
+              objectFit="cover"
+              borderRadius={5}
+            />
+          </Box>
+          <Flex gap={10} align="center">
+            <Box>
+              <Text fontSize={"21px"}>{item.title}</Text>
+              <Text fontSize={"17px"} py={3}>
+                Author: {item.author}
+              </Text>
+
+              <Text
+                onClick={() => handleBookDelete(item._id)}
+                fontSize={"26px"}
+                color={"red.500"}
+                cursor="pointer"
+              >
+                <AiOutlineDelete />
+              </Text>
+            </Box>
+            <Box>
+              <Text fontSize={"21px"}>Shs {item.price}</Text>
+            </Box>
+          </Flex>
+        </Flex>
+      ))}
+    </Flex>
+  );
+}
+
+export default AdminBook;
