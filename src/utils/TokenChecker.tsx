5ef2ce54987587cf145f56c2076101867ca6f73e
isiagi <isiagigeofrey0@gmail.com>
2023-04-27 15:43:00 +0300

    refactor: Add token checker

---
+++ src/utils/TokenChecker.tsx
@@ -0,0 +1,28 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-nocheck
+import { useState, useEffect } from 'react';
+
+function TokenChecker({ key }) {
+  const [token, setToken] = useState(localStorage.getItem(key));
+
+  useEffect(() => {
+    const storageChange = (event) => {
+      if (event.key === key) {
+        const newToken = event.newValue;
+        if (newToken !== token) {
+          setToken(newToken);
+        }
+      }
+    };
+
+    window.addEventListener('storage', storageChange);
+
+    return () => {
+      window.removeEventListener('storage', storageChange);
+    };
+  }, [key, token]);
+
+  return Boolean(token);
+}
+
+export default TokenChecker;
