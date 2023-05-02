d68dc9e6bf07cfd36b12d26d6f73d0054c2bbb63
isiagi <isiagigeofrey0@gmail.com>
2023-05-02 10:42:22 +0300

    chore: Make video on mobile, add poster img

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,20 +1,35 @@
 import { Box, Button, Flex, Text } from "@chakra-ui/react";
-import vid from "../../assets/vid.mp4"
+import vid from "../../assets/vid.mp4";
 
 function Hero() {
   return (
-    <Box
-      h={"100vh"}
-      position={'relative'}
-    >
-      <video autoPlay loop muted style={{objectFit: 'cover',height: '100%', width: '100%'}}>
-        <source src={vid} type="video/mp4"/>
+    <Box h={"100vh"} position={"relative"}>
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        poster="https://media.istockphoto.com/id/1396463217/photo/light-bulbs-glowing-with-book-the-idea-of-reading-books-knowledge-and-searching-for-new-ideas.jpg?b=1&s=170667a&w=0&k=20&c=5cSpQ_aQ9yaKJj85SNN8MDrEbJhRJqM4zTGG6NEu5qA="
+        style={{ objectFit: "cover", height: "100%", width: "100%" }}
+      >
+        <source src={vid} type="video/mp4" />
       </video>
-      <Box pl={36} h={'100%'} w={'100%'} position={'absolute'} top={0} bg={'rgba(0, 0, 0, 0.6)'}>
-        <Flex align={'center'} h='100%'>
+      <Box
+        pl={36}
+        h={"100%"}
+        w={"100%"}
+        position={"absolute"}
+        top={0}
+        bg={"rgba(0, 0, 0, 0.6)"}
+      >
+        <Flex align={"center"} h="100%">
           <Box>
-            <Text fontSize='4xl' color={"#fff"}>Welcome To BookShop</Text>
-            <Text fontSize='2xl' color={"#fff"}>Broswer over 200 Books</Text>
+            <Text fontSize="4xl" color={"#fff"}>
+              Welcome To BookShop
+            </Text>
+            <Text fontSize="2xl" color={"#fff"}>
+              Broswer over 200 Books
+            </Text>
             <Button>Broswer Collection</Button>
           </Box>
         </Flex>
